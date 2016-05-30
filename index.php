5655531eadcaeea7925fca6d842b10ba62ace333
Sato Yusuke <k014c1242@it-neec.jp>
2016-05-30 14:05:50 +0900

    first

--- index.php
+++ index.php
@@ -7,6 +7,22 @@
 </head>
 
 <body>
+
+<?php
+
+  $dsn = 'mysql:dbname=todolist;host=localhost;charset=UTF-8'
+  $user = 'root'
+  $password = 'root'
+  $dbh = new PDO($dsn,$user,$password);
+  $dbh->query('SET NAMES utf8');
+  $dbh=>setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+
+  if(isset($_GET['add'])){
+    $item = $_GET['item'];
+  }
+  ?>
+
+  <h1>Todolist</h1>
   <table border ="１"　width="500" cellspacing="0" cellpadding="5" id="table1">
   <tr>
     <td><input type = "text"name="name"size="30"maxlength="20"></td>
@@ -14,14 +30,14 @@
   </tr>
 
 
-  <table border ="2"  cellspacing="0" cellpadding="5" id="table2">
+  <table border ="2"  id="table2">
   <hr><br>
   <tr>
     <td class="task"><li name="name" value="" size="10" maxlength="20"/></td>
     <td><input type="image" src="ico_ashcan1_9.gif" name="delete" alt="削除"></td>
   </tr>
 
-
 </body>
 
-<html>
+</html>
+
