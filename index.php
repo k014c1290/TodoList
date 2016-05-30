f1ab93f5f8ad05bfb5bb7584a6443aa735a2b84a
Sato Yusuke <k014c1242@it-neec.jp>
2016-05-30 13:58:40 +0900

    first

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8"/>
+<title>Todolist</title>
+
+</head>
+
+<body>
+  <table border ="１"　width="500" cellspacing="0" cellpadding="5" id="table1">
+  <tr>
+    <td><input type = "text"name="name"size="30"maxlength="20"></td>
+    <td><input type="button"value="add"></td>
+  </tr>
+
+
+  <table border ="2"  cellspacing="0" cellpadding="5" id="table2">
+  <hr><br>
+  <tr>
+    <td class="task"><li name="name" value="" size="10" maxlength="20"/></td>
+    <td><input type="image" src="ico_ashcan1_9.gif" name="delete" alt="削除"></td>
+  </tr>
+
+
+</body>
+
+<html>
